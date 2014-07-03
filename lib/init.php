f52f6f52458a29de7e2ae8c2c4331da15c295b05
Det Hsieh <dethsieh@kkbox.com>
2014-07-04 02:59:42 +0800

    another rows

--- lib/init.php
+++ lib/init.php
@@ -8,3 +8,13 @@ $rows = array(
     array('id' => 6, 'type' => 1, 'name' => '6th',),
     array('id' => 7, 'type' => 3, 'name' => '7...zzz',),
 );
+
+$rows2 = array(
+    array('id' => 1, 'title' => '吳大', 'name' => 'Wing', 'sex' => 1,),
+    array('id' => 2, 'title' => '史黛希大大', 'name' => 'Stacey', 'sex' => 0,),
+    array('id' => 3, 'title' => '菲安大大', 'name' => 'Fion', 'sex' => 0,),
+    array('id' => 4, 'title' => '把它尼醬～', 'name' => 'Butany', 'sex' => 1,),
+    array('id' => 5, 'title' => '毫無反應的Det', 'name' => 'Det', 'sex' => 1,),
+    array('id' => 6, 'title' => 'みんなのＣＱＤ', 'name' => 'Wesley', 'sex' => 1,),
+    array('id' => 7, 'title' => '名偵探步美', 'name' => 'Vera', 'sex' => 0,),
+);
