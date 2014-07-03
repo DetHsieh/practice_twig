f30c7b2e59b0e7d07cd6e7f55030ccbe0b429e53
Det Hsieh <dethsieh@kkbox.com>
2014-07-03 21:02:14 +0800

    fake rows

---
+++ lib/init.php
@@ -0,0 +1,10 @@
+<?php
+$rows = array(
+    array('id' => 1, 'type' => 1, 'name' => '第一個',),
+    array('id' => 2, 'type' => 2, 'name' => '第二個',),
+    array('id' => 3, 'type' => 3, 'name' => 'THREEEEEEE',),
+    array('id' => 4, 'type' => 1, 'name' => '4th',),
+    array('id' => 5, 'type' => 2, 'name' => '5th',),
+    array('id' => 6, 'type' => 1, 'name' => '6th',),
+    array('id' => 7, 'type' => 3, 'name' => '7...zzz',),
+);
