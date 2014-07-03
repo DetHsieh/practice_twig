579d8ce6d7c75fc69493042039e65b1705533499
Det Hsieh <dethsieh@kkbox.com>
2014-07-03 21:06:33 +0800

    [PHP] demo_01

---
+++ public/demo_01.php
@@ -0,0 +1,11 @@
+<?php
+$loader = require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../lib/init.php';
+
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/../templates');
+$twig = new Twig_Environment($loader, array('strict_variables' => true));
+
+$array_data = array(
+);
+
+echo $twig->render('demo_01.html.twig', $array_data); // 放在VIEW裡面的檔案
