f5068d2a74e4f35f3646d3c0e7604a406e4fe662
Det Hsieh <dethsieh@kkbox.com>
2014-07-04 13:02:26 +0800

    page 9

---
+++ public/demo_09.php
@@ -0,0 +1,15 @@
+<?php
+$autoloader = require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../lib/init.php';
+require __DIR__ . '/../lib/extension.php';
+
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/../templates');
+$twig = new Twig_Environment($loader, array('strict_variables' => true));
+$twig->addExtension(new Demo_Twig_Extension());
+
+$array_data = array(
+    'rows' => $rows,
+    'num' => 9,
+);
+
+echo $twig->render('demo_09.html.twig', $array_data); // 放在VIEW裡面的檔案
